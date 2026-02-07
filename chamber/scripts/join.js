feb70c78b411ea1b6a8af9bfeece64d434840923
LuckyRambly <ramonrayopro@gmail.com>
2026-02-07 14:18:55 -0400

    Improved accesibility, scripts and design in join.html and thankyou.html

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -10,19 +10,32 @@ document.addEventListener("DOMContentLoaded", () => {
         card.classList.add('animate-slide-up');
     });
 
+    const openButtons = document.querySelectorAll('.info-btn');
+    openButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const modalId = button.getAttribute('data-modal');
+            const modal = document.getElementById(modalId);
+            if (modal) modal.showModal();
+        });
+    });
+
+    const closeButtons = document.querySelectorAll('.close-modal');
+    closeButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            button.closest('dialog').close();
+        });
+    });
+
     const allModals = document.querySelectorAll('dialog');
-    
     allModals.forEach(modal => {
         modal.addEventListener('click', (event) => {
             const rect = modal.getBoundingClientRect();
-            
             const isInDialog = (
                 event.clientX >= rect.left &&
                 event.clientX <= rect.right &&
                 event.clientY >= rect.top &&
                 event.clientY <= rect.bottom
             );
-            
             if (!isInDialog) {
                 modal.close();
             }
